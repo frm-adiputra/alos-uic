1beeaddb5a08244850b770c0a6d1b76a754c7af4
Firmansyah Adiputra <frm.adiputra@gmail.com>
2019-01-10 17:45:00 +0700

    Initial commit

---
+++ src/lib-dev.vue
@@ -0,0 +1,23 @@
+<script>
+import { AlosUicSample } from './entry';
+export default {
+    name:  'AlosUicLibDev', // vue library dev component
+    components: {
+        AlosUicSample,
+    },
+}
+</script>
+
+<template>
+    <div class="alos-uic-lib-dev">
+        <alos-uic-sample></alos-uic-sample>
+    </div>
+</template>
+
+<style scoped>
+    .alos-uic-lib-dev {
+        display: block;
+        width: 400px;
+        margin: 25px auto;
+    }
+</style>
