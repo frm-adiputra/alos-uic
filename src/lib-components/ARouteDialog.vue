4402cd825d3e02b2a268a78f0895c9c0cb425dc6
Firmansyah Adiputra <frm.adiputra@gmail.com>
2019-02-04 00:44:56 +0700

    Add props

--- src/lib-components/ARouteDialog.vue
+++ src/lib-components/ARouteDialog.vue
@@ -1,11 +1,19 @@
 <template lang="pug">
-v-dialog(:value="dlg" @input="dlgChanged($event)")
+v-dialog(:value="dlg" @input="dlgChanged($event)"
+	:width="width"
+	:dark="dark"
+	:light="light"
+	:disabled="disabled"
+	:persistent="persistent"
+	:max-width="maxWidth"
+)
 	slot
 </template>
 
 <script>
 export default {
 	name: 'ARouteDialog',
+	props: ['width', 'dark', 'light', 'disabled', 'persistent', 'maxWidth'],
 	data() {
 		return {
 			dlg: false
